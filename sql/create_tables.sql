ecd8e64d67e5a08c933ac6cd245162404ac5a1c3
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2023-08-11 09:12:23 -0500

    Started to add SQL Schemas

---
+++ sql/create_tables.sql
@@ -0,0 +1,28 @@
+-- Creating RoutingEventHistory --
+CREATE TABLE IF NOT EXISTS RoutingEventHistory (
+    id INT NOT NULL,
+    dateTimeReceived TIMESTAMP NOT NULL,
+    dateTimeSent TIMESTAMP NOT NULL,
+    receiver varchar(128) NOT NULL,
+    sent varchar(128) NOT NULL,
+    payload varchar(2056)
+);
+
+--    private int id;
+--
+--    @Column(name = "DateReceived", nullable = false)
+--    private LocalDateTime dateReceived;
+--
+--    @Column(name = "DateSent", nullable = false)
+--    private LocalDateTime dateSent;
+--
+--    @Column(name = "Sender", nullable = false)
+--    private String sender; //This should be some number or key
+--
+--    @Column(name = "Receiver", nullable = false)
+--    private String receiver; //This should be some number or key
+--
+--    @Column(name = "Payload")
+--    // JSON Object serialized into a string
+--    private String payload;
+)
